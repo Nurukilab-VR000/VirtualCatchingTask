bb46cfa14c1712196c33e027f4c811687da46796
Nurukilab-VR000 <k9059303@kadai.jp>
2022-10-25 19:12:52 +0900

    1st commit

---
+++ VitualCatchingTask/Assets/Ball/Motion_Ball5.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class Motion_Ball5 : MonoBehaviour
+{
+    [SerializeField] Transform endPos1;  //終点座標
+    private const float gravity = -9.8f;    //重力
+    Transform endPos;
+
+    void Start()
+    {
+        endPos = endPos1;
+        float speedRate = 2.0f;   //滞空時間を基準とした移動速度倍率
+        float flightTime = 1.0f;  //滞空時間
+        CSVSave(speedRate, "2022xxxx_RO_xxxx");
+        StartCoroutine(Jump(endPos.position, flightTime, speedRate, gravity));
+    }
+
+    // 現在位置からendPosへの放物運動　
+    private IEnumerator Jump(Vector3 endPos, float flightTime, float speedRate, float gravity)
+    {
+        var startPos = transform.position; // 初期位置
+        var diffY = (endPos - startPos).y; // 始点と終点のy成分の差分
+        var vn = (diffY - gravity * 0.5f * flightTime * flightTime) / flightTime; // 鉛直方向の初速度vn
+
+        // 放物運動
+        for (var t = 0f; t < flightTime; t += (Time.deltaTime * speedRate))
+        {
+            var p = Vector3.Lerp(startPos, endPos, t / flightTime);   //水平方向の座標を求める (x,z座標)
+            p.y = startPos.y + vn * t + 0.5f * gravity * t * t;  // 鉛直方向の座標 y
+            transform.position = p;
+            yield return null; //1フレーム経過
+        }
+        // 終点座標へ補正
+        transform.position = endPos;
+    }
+
+    //CSV保存するための関数
+    private void CSVSave(float data, string fileName)
+    {
+        //ファイル書き込み
+        FileInfo fi;
+        DateTime now = DateTime.Now;
+        StreamWriter sw;
+
+        fi = new FileInfo(Application.dataPath + "/CSV/" + fileName + ".csv");
+        sw = fi.AppendText();
+        sw.Write("," + data);
+        sw.Flush();
+        sw.Close();
+        Debug.Log("Save Completed");
+    }
+}
---
+++ VitualCatchingTask/Assets/Script/DestroySphere.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+public class DestroySphere : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // GenerateTimeDataを作って、CSVSaveの関数に引数として渡す
+        var BrokenTimeData = Time.time;
+        var CollisionObj = collision.gameObject.name;
+
+        CSVApp(CollisionObj, Time.time, "2022xxxx_RO_xxxx");
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            // pos_x,pos_y,pos_zを作って、CSVSaveの関数に引数として渡す
+            float pos_x = contact.point.x;
+            float pos_y = contact.point.y;
+            float pos_z = contact.point.z;
+            float[] Pos = new float[] { pos_x, pos_y, pos_z };
+            //CSVSave(Pos, "DestroyPos");
+        }
+        Destroy(gameObject);
+
+    }
+
+    //CSV保存するための関数
+    private void CSVApp(string name ,float data, string fileName)
+    {
+        //ファイル書き込み
+        FileInfo fi;
+        DateTime now = DateTime.Now;
+        StreamWriter sw;
+
+        fi = new FileInfo(Application.dataPath + "/CSV/" + fileName + ".csv");
+        sw = fi.AppendText();
+        sw.Write(","+ name + ",");
+        sw.WriteLine(data);
+        sw.Flush();
+        sw.Close();
+    }
+
+    //CSV保存するための関数
+    private void CSVSave(float[] data, string fileName)
+    {
+        //ファイル書き込み
+        FileInfo fi;
+        DateTime now = DateTime.Now;
+        StreamWriter sw;
+
+        fi = new FileInfo(Application.dataPath + "/CSV/" + fileName + ".csv");
+        sw = fi.AppendText();
+        for (int i = 0; i <= 2; i++)
+        {
+            sw.Write(data[i] + ",");
+        }
+        sw.WriteLine("");
+        sw.Flush();
+        sw.Close();
+    }
+}
