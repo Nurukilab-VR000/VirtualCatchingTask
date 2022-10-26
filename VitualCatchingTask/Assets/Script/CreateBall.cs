bf08894c8270739bbbdcfa0c21d62a7691acc15e
Nurukilab-VR000 <k9059303@kadai.jp>
2022-10-26 17:00:35 +0900

    20221026 commit

--- VitualCatchingTask/Assets/Script/CreateBall.cs
+++ VitualCatchingTask/Assets/Script/CreateBall.cs
@@ -7,31 +7,31 @@ using System.IO;
 
 public class CreateBall : MonoBehaviour
 {
-    public GameObject M_Ball1;   //GameObject取得 
-    public GameObject M_Ball2;   //GameObject取得
-    public GameObject M_Ball3;   //GameObject取得
-    public GameObject M_Ball4;   //GameObject取得
-    public GameObject M_Ball5;   //GameObject取得
-    public GameObject M_Ball6;   //GameObject取得
-    public GameObject M_Ball7;   //GameObject取得
-    public GameObject M_Ball8;   //GameObject取得
-    public GameObject M_Ball9;   //GameObject取得
-    public GameObject M_Ball10;   //GameObject取得
-    public GameObject M_Ball11;   //GameObject取得
-    public GameObject M_Ball12;   //GameObject取得
-    public GameObject M_Ball13;   //GameObject取得
-    public GameObject M_Ball14;   //GameObject取得
-    public GameObject M_Ball15;   //GameObject取得
-    public GameObject M_Ball16;   //GameObject取得
-    public GameObject M_Ball17;   //GameObject取得
-    public GameObject M_Ball18;   //GameObject取得
-    public GameObject M_Ball19;   //GameObject取得
-    public GameObject M_Ball20;   //GameObject取得
-    public GameObject M_Ball21;   //GameObject取得
-    public GameObject M_Ball22;   //GameObject取得
-    public GameObject M_Ball23;   //GameObject取得
-    public GameObject M_Ball24;   //GameObject取得
-    public GameObject M_Ball25;   //GameObject取得
+    public GameObject VCT_Ball1;   //GameObject取得 
+    public GameObject VCT_Ball2;   //GameObject取得
+    public GameObject VCT_Ball3;   //GameObject取得
+    public GameObject VCT_Ball4;   //GameObject取得
+    public GameObject VCT_Ball5;   //GameObject取得
+    public GameObject VCT_Ball6;   //GameObject取得
+    public GameObject VCT_Ball7;   //GameObject取得
+    public GameObject VCT_Ball8;   //GameObject取得
+    public GameObject VCT_Ball9;   //GameObject取得
+    public GameObject VCT_Ball10;   //GameObject取得
+    public GameObject VCT_Ball11;   //GameObject取得
+    public GameObject VCT_Ball12;   //GameObject取得
+    public GameObject VCT_Ball13;   //GameObject取得
+    public GameObject VCT_Ball14;   //GameObject取得
+    public GameObject VCT_Ball15;   //GameObject取得
+    public GameObject VCT_Ball16;   //GameObject取得
+    public GameObject VCT_Ball17;   //GameObject取得
+    public GameObject VCT_Ball18;   //GameObject取得
+    public GameObject VCT_Ball19;   //GameObject取得
+    public GameObject VCT_Ball20;   //GameObject取得
+    public GameObject VCT_Ball21;   //GameObject取得
+    public GameObject VCT_Ball22;   //GameObject取得
+    public GameObject VCT_Ball23;   //GameObject取得
+    public GameObject VCT_Ball24;   //GameObject取得
+    public GameObject VCT_Ball25;   //GameObject取得
 
     private void Start()
     {
@@ -84,103 +84,103 @@ public class CreateBall : MonoBehaviour
 
             if (count == 1)
             {
-                Instantiate(M_Ball1, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball1, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 2)
             {
-                Instantiate(M_Ball2, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball2, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 3)
             {
-                Instantiate(M_Ball3, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball3, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 4)
             {
-                Instantiate(M_Ball4, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball4, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 5)
             {
-                Instantiate(M_Ball5, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball5, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 6)
             {
-                Instantiate(M_Ball6, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball6, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 7)
             {
-                Instantiate(M_Ball7, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball7, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 8)
             {
-                Instantiate(M_Ball8, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball8, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 9)
             {
-                Instantiate(M_Ball9, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball9, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 10)
             {
-                Instantiate(M_Ball10, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball10, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 11)
             {
-                Instantiate(M_Ball11, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball11, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 12)
             {
-                Instantiate(M_Ball12, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball12, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 13)
             {
-                Instantiate(M_Ball13, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball13, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 14)
             {
-                Instantiate(M_Ball14, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball14, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 15)
             {
-                Instantiate(M_Ball15, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball15, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 16)
             {
-                Instantiate(M_Ball16, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball16, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 17)
             {
-                Instantiate(M_Ball17, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball17, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 18)
             {
-                Instantiate(M_Ball18, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball18, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 19)
             {
-                Instantiate(M_Ball19, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball19, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 20)
             {
-                Instantiate(M_Ball20, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball20, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 21)
             {
-                Instantiate(M_Ball21, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball21, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 22)
             {
-                Instantiate(M_Ball22, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball22, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 23)
             { 
-                Instantiate(M_Ball23, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball23, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 24)
             {
-                Instantiate(M_Ball24, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball24, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             if (count == 25)
             {
-                Instantiate(M_Ball25, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
+                Instantiate(VCT_Ball25, new Vector3(x, y, z), Quaternion.identity); //ターゲット生成
             }
             k++;
         }
