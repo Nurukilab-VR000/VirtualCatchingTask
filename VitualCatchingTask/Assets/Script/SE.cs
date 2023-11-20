59c938641ce3198611de8abe20cd7235e97d317e
warabinbin <mochi.warabii@gmail.com>
2023-11-20 22:29:03 +0900

    add Vibration

--- VitualCatchingTask/Assets/Script/SE.cs
+++ VitualCatchingTask/Assets/Script/SE.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+
+//SEとVibration
 
 public class SE : MonoBehaviour
 {
@@ -12,102 +16,129 @@ public class SE : MonoBehaviour
         if ((collision.gameObject.name == "Ball1(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+
+            //Execute(float secondsFromNow, float durationSeconds, float frequency, float amplitude)
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball2(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball3(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball4(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball5(Clone)"))
         {
-            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position); 
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball6(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball7(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball8(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball9(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball10(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball11(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball12(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball13(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball14(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball15(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball16(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball17(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball18(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball19(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball20(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball21(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball22(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball23(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball24(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
         if ((collision.gameObject.name == "Ball25(Clone)"))
         {
             AudioSource.PlayClipAtPoint(clip, transform.position);
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.1f, 50, 1);
         }
     }
 
